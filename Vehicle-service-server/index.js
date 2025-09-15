3e08b217916cf334490f6d428451fd5d9cd4891f
2441289 <pradoshp91@gmail.com>
2025-09-15 21:54:39 +0530

    change the password in index.js

--- Vehicle-service-server/index.js
+++ Vehicle-service-server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = mysql.createConnection({ 
   host: 'localhost', 
   user: 'root',  
-  password: '19263543', 
+  password: 'enter the password', 
   database: 'sb'
 });
 
