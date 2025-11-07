0324ad4ba19b8f300f1c64ee5bb389b338171440
Rithigesh <rithigeshrithick303@gmail.com>
2025-11-07 09:21:55 +0530

    Updated service register

--- src/components/services/RegisterServices.jsx
+++ src/components/services/RegisterServices.jsx
@@ -4,20 +4,8 @@ import axios from "axios";
 const BASE = import.meta.env.VITE_SERVER_URL;
 const AUTH_URL = `${BASE}/app1/api/v1/auth/register`;
 class RegisterServices{
-    getAuth(){
-        return axios.get(`${api}/app2/api`);
-    }
-    getAuthById(id){
-        return axios.get(`${api}/${id}`);
-    }
     addAuth(credentials){
         return axios.post(`${AUTH_URL}` , credentials);
-    }
-    deleteAuth(id){
-        return axios.delete(`${api}/${id}`);
-    }
-    editAuth(id , credentials){
-        return axios.put(`${api}/${id}` , credentials);
     }
 }
 
